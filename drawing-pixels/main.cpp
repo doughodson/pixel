b43a8ead975b3f48ba4378295ca3bbafbabb3f13
Doug Hodson <doug@sidechannel.net>
2020-12-26 10:53:07 -0500

    added drawing pixels example

---
+++ drawing-pixels/main.cpp
@@ -0,0 +1,31 @@
+
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+class BreakOut : public olc::PixelGameEngine
+{
+public:
+	BreakOut()
+	{
+		sAppName = "TUTORIAL - BreakOut Clone";
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		return true;
+	}
+};
+
+int main()
+{
+	BreakOut demo;
+	if (demo.Construct(512, 480, 2, 2))
+		demo.Start();
+	return 0;
+}
---
+++ drawing-pixels/mk.sh
@@ -0,0 +1,3 @@
+
+g++ -o drawing-pixels main.cpp -lX11 -lGL -pthread -lpng -lstdc++fs -std=c++17 -I../include
+
