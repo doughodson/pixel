b41af1559ced783bffcdb0c1643e83f8133ab125
Doug Hodson <doug@sidechannel.net>
2020-12-26 11:01:17 -0500

    added Makefiles

--- drawing-pixels/mk.sh
+++
@@ -1,3 +0,0 @@
-
-g++ -o drawing-pixels main.cpp -lX11 -lGL -pthread -lpng -lstdc++fs -std=c++17 -I../include
-
--- hello-world/mk.sh
+++
@@ -1,3 +0,0 @@
-
-g++ -o hello-world main.cpp -lX11 -lGL -pthread -lpng -lstdc++fs -std=c++17 -I../include
-
