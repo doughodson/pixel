42997ac2e1c0331dfd43acfd61b1754b5719cf27
Doug Hodson <doug@sidechannel.net>
2020-12-27 16:05:38 -0500

    update

--- sprites/main.cpp
+++ sprites/main.cpp
@@ -42,8 +42,23 @@ public:
          }
       }
 
+      for (int y = 0; y < 30; y++) {
+         for (int x = 0; x < 24; x++) {
+            if (x == 0 || y == 0 || x == 23)
+               blocks[y * 24 + x] = 10;
+            else
+               blocks[y * 24 + x] = 0;
+
+            if (x > 2 && x <= 20 && y > 3 && y <= 5)
+               blocks[y * 24 + x] = 1;
+            if (x > 2 && x <= 20 && y > 5 && y <= 7)
+               blocks[y * 24 + x] = 2;
+            if (x > 2 && x <= 20 && y > 7 && y <= 9)
+               blocks[y * 24 + x] = 3;
+	      }
+      }
       // load sprite
-      sprTile = std::make_unique<olc::Sprite>("./gfx/tut_tile.png");
+      sprTile = std::make_unique<olc::Sprite>("./gfx/tut_tiles.png");
       return true;
    }
 
@@ -51,18 +66,28 @@ public:
    {
       // draw screen
       Clear(olc::DARK_BLUE);
+      SetPixelMode(olc::Pixel::MASK);  // don't draw pixels which has any transparency
       for (int y = 0; y < 30; y++)for (int y = 0; y < 30; y++) {
          for (int x = 0; x < 24; x++) {
             switch (blocks[y * 24 + x]) {
-            case 0: // do nothing
-               break;
-            case 10: // draw boundary
-               //FillRect(olc::vi2d(x, y) * vBlockSize, vBlockSize, olc::WHITE);
-               DrawSprite(olc::vi2d(x, y) * vBlockSize, sprTile.get());
-            break;
+               case 0: // do nothing
+                  break;
+               case 10: // draw boundary
+                  DrawPartialSprite(olc::vi2d(x, y) * vBlockSize, sprTile.get(), olc::vi2d(0, 0) * vBlockSize, vBlockSize);
+                  break;
+               case 1: // draw red block
+                  DrawPartialSprite(olc::vi2d(x, y) * vBlockSize, sprTile.get(), olc::vi2d(1, 0) * vBlockSize, vBlockSize);
+                  break;
+               case 2: // draw green block
+                  DrawPartialSprite(olc::vi2d(x, y) * vBlockSize, sprTile.get(), olc::vi2d(2, 0) * vBlockSize, vBlockSize);
+                  break;
+               case 3: // draw yellow block
+                  DrawPartialSprite(olc::vi2d(x, y) * vBlockSize, sprTile.get(), olc::vi2d(3, 0) * vBlockSize, vBlockSize);
+                  break;
             }
          }
       }
+      SetPixelMode(olc::Pixel::NORMAL); // draw all pixels
       return true;
    }
 };
