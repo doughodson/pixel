3b311d9b309bdc21bfd1b9c3a329b870baaa9ff0
Doug Hodson <doug@sidechannel.net>
2020-12-27 14:27:47 -0500

    updated sprite example

--- sprites/main.cpp
+++ sprites/main.cpp
@@ -30,58 +30,33 @@ private:
 public:
 	bool OnUserCreate() override
 	{
-      std::srand(100);
+      blocks = std::make_unique<int[]>(24*30);
+      for (int y=0; y<30; y++) {
+         for( int x=0; x < 24; x++) {
+            if (x == 0 || y == 0 || x == 23)
+               blocks[y*24+x] = 10;
+            else
+               blocks[y*24+x] = 0;
+         }
+      }
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-      // handle user input
-      if (GetKey(olc::Key::LEFT).bHeld) fBatPos -= fBatSpeed * fElapsedTime;
-      if (GetKey(olc::Key::RIGHT).bHeld) fBatPos += fBatSpeed * fElapsedTime;
-
-      // constrain bat position (paddle)
-      if (fBatPos < 11.0f) fBatPos = 11.0f;
-		if (fBatPos + fBatWidth > float(ScreenWidth()) - 10.0f) fBatPos = float(ScreenWidth()) - 10.0f - fBatWidth;
-
-      // update ball
-      vBall += vBallVel * fElapsedTime;
-
-      // crude arena detection - this approach sucks
-      if (vBall.y <= 10.0f) vBallVel.y *= -1.0f;
-      if (vBall.x <= 10.0f) vBallVel.x *= -1.0f;
-      if (vBall.x >= float(ScreenWidth()) - 10.0f) vBallVel.x *= -1.0f;
-
-      // check bat
-      if (vBall.y >= (float(ScreenHeight()) - 20.0f) && (vBall.x > fBatPos) && (vBall.x < fBatPos + fBatWidth)) vBallVel.y *= -1.0f;
-
-      // check if ball has gone off screen
-      if (vBall.y > ScreenHeight()) {
-         // reset ball location
-         vBall = { 200.0f, 200.0f };
-         // choose random direction
-         float fAngle = (float(std::rand()) / float(RAND_MAX)) * 2.0f * 3.14159f;
-         vBallVel = { 300.0f * std::cos(fAngle), 300.0f * std::sin(fAngle) };
-      }
-
-      if (GetMouseWheel() > 0) fBallRadius += 1.0f;
-      if (GetMouseWheel() < 0) fBallRadius -= 1.0f;
-      if (fBallRadius < 5.0f) fBallRadius = 5.0f;
-
-      // erase previous frame
+      // draw screen
       Clear(olc::DARK_BLUE);
-
-      // draw boundary
-      DrawLine(10, 10, ScreenWidth() - 10, 10, olc::YELLOW);    // top edge
-      DrawLine(10, 10, 10, ScreenHeight() - 10, olc::YELLOW);   // left edge
-      DrawLine(ScreenWidth() - 10, 10, ScreenWidth() - 10, ScreenHeight() - 10, olc::YELLOW);  // right edge
-
-      // draw bat
-	   FillRect(int(fBatPos), ScreenHeight() - 20, int(fBatWidth), 10, olc::GREEN);
-
-	   // draw ball
-      FillCircle(vBall, int(fBallRadius), olc::CYAN);
-
+      for (int y = 0; y < 30; y++)for (int y = 0; y < 30; y++) {
+         for (int x = 0; x < 24; x++) {
+            switch (blocks[y * 24 + x]) {
+            case 0: // do nothing
+               break;
+            case 10: // draw Boundary
+               FillRect(olc::vi2d(x, y) * vBlockSize, vBlockSize, olc::WHITE);
+            break;
+            }
+         }
+      }
       return true;
 	}
 };
