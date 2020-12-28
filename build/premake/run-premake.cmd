abb83d01c1d140a54322c87ba150aaff77b2a12c
Doug Hodson <doug@sidechannel.net>
2020-12-28 11:59:08 -0500

    updated premake build scripts

--- build/premake/run-premake.cmd
+++ build/premake/run-premake.cmd
@@ -1,12 +1,9 @@
 rem
 rem delete old solution/workspace files
 rem
-rmdir /S /Q ..\gmake
-rmdir /S /Q ..\vs2017
 rmdir /S /Q ..\vs2019
 rmdir /S /Q ..\obj
 rem
 rem create solution/workspace files
 rem
-premake5-alpha15.exe --os=windows --file=premake5.lua vs2017
 premake5-alpha15.exe --os=windows --file=premake5.lua vs2019
